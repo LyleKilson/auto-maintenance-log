0d193e3ac448adbf29ce1eac204769c0fee02459
Caroline Phillips <Caroline@Carolines-MacBook-Air.local>
2021-04-29 21:03:41 -0600

    controllers

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,12 @@
 // api routes middleware goes into this file to be exported to controllers/index.js
+const router = require('express').Router();
+
+const  maintenanceRoutes = require('./maintenance-routes');
+const vehicleRoutes = require ('./vehicle-routes');
+const ownerRoutes = require('./owner-routes');
+
+router.use('/maintenance', maintenanceRoutes);
+router.use('/vehicles', vehicleRoutes);
+router.use('/owners', ownerRoutes );
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,12 @@
 // dashboard, home, and api routes middleware for exportation on this file 
+const router = require('express').Router();
+
+const apiRoutes = require('./api/');
+const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboard-routes');
+
+router.use('/dashboard', dashboardRoutes);
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
