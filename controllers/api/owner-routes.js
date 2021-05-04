1f2087d5a3721400e9aa5217f4fadf786e9e8e58
sebastian <sebastian bowen>
2021-05-03 22:49:51 -0600

    feature/api-routes

--- controllers/api/owner-routes.js
+++ controllers/api/owner-routes.js
@@ -11,4 +11,36 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+    Owner.findOne({
+        attributes: {exclude: ['password']},
+        where: {
+            id: req.params.id 
+         },
+          include: [
+            {
+                model: Vehicle,
+                attributes: ['id', 'make', 'model', 'year', 'created_at' ],
+                include: MaintLog,
+                attributes: ['id']
+            }
+        ]
+    })
+    .then(ownerData => {
+        if(!ownerData) {
+            res.status(400).json({message: 'no owner has this ID'})
+            return;
+        }
+        res.json(ownerData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
+
+
+
+/*'last_oil_change', 'last_tire_rotation', 'last_spark_plugs', 'last_ait_filter', 'notes', 'vehicle_id'] */ 
