5e48f9127862943ecdd3a1f2d1d390ca26eeac61
sebastian <sebastian bowen>
2021-05-05 00:41:19 -0600

    dashboard rendering in browser

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,9 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
+const dashBoardRoutes = require('./dashboard-routes');
 
+router.use('/dashboard', dashBoardRoutes)
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
