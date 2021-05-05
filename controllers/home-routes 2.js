8589a3aaa44b1e4982bacb179744abfe8aa3e653
Lyle Kilson <lylekilsongithub@gmail.com>
2021-05-05 14:12:09 -0600

    test

---
+++ controllers/home-routes 2.js
@@ -0,0 +1,15 @@
+// routes for homepage
+const router = require('express').Router();
+const sequelize= require('../config/connection');
+const { Owner, Vehicle, MaintLog } = require('../models');
+
+
+
+router.get('/', (req, res) => {
+  res.render('homepage');
+});
+
+router.get('/login', (req, res) => {
+  res.render('login');
+  });
+module.exports = router;
--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,3 @@
 DROP DATABASE IF EXISTS vehicle_maintenance_db;
 
 CREATE DATABASE  vehicle_maintenance_db;
-
-CREATE TABLE vehicle {
-    id INTEGER NOT NULL,
-    vehicle
