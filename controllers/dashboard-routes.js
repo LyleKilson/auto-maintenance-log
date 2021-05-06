3b5362cf2b5364dc1db3a9ef979128c90e91db1d
sebastian <sebastian bowen>
2021-05-06 01:17:25 -0600

    vehicles now rendering on dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -4,8 +4,26 @@ const sequelize= require('../config/connection');
 const { Owner, Vehicle, MaintLog } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('dashboard',{loggedIn: true})
-})
+    Vehicle.findAll({
+        where: {
+            owner_id: req.session.owner_id
+        },
+        attributes: [
+                  'id',
+                  'make',
+                  'model',
+                  'year',
+                  'created_at'
+                    ] 
+    }).then(dbVehicleData => {
+        const vehicle = dbVehicleData.map(vehicle => vehicle.get({plain: true}));
+        res.render('dashboard',{vehicle, loggedIn: true})
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
 
