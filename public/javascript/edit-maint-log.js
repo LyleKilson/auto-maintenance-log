424d10ad76500ded21f4126a26f5d8527be49c8a
L-Barrios <luisiannabarrios@gmail.com>
2021-05-08 20:32:36 -0600

    tried edit log js

--- public/javascript/edit-maint-log.js
+++ public/javascript/edit-maint-log.js
@@ -1,1 +1,35 @@
 // front end js for editing a maintenance log
+async function editMaint(event) {
+    event.preventDefault();
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1];
+
+    const oil = document.querySelector('input[name="oil"]').value.trim();
+    const tire = document.querySelector('input[name="tire"]').value.trim();
+    const plugz = document.querySelector('input[name="spark"]').value.trim();
+    const filter = document.querySelector('input[name="air"]').value.trim();
+    const notes = document.querySelector('input[name="notes"]').value.trim();
+
+    const response = await fetch('/api/maintlogs/${id}', {
+        method: 'PUT', 
+
+        body:  JSON.stringify({ notes,   
+            oil,
+            tire,
+            plugz,
+            filter, 
+           
+        }),
+        headers: {'Content-Type': 'application/json'}
+});
+
+    if(response.ok) {
+    console.log(response);
+    }else{
+        alert('Error!' + response.statusText);
+    }
+
+    }
+ 
+
+document.querySelector('.log').addEventListener('submit', editMaint);
